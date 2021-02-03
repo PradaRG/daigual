00e30b658b4c414d220e92e04db1fb648637fa9a
Alfonso Ramat <alfonso.ramat@gmail.com>
2021-02-03 00:22:59 -0300

    login window

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [password, setpassword] = useState('');
+
+  function nameChange(event) {
+    setUser(event.target.value);
+  }
+  function passwordChange(event) {
+    setpassword(event.target.value);
+  }
+
+  function enviarFormulario(event) {
+    event.preventDefault();
+    console.log(`usuario: ${user}, contraseña:${password}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <div className="login-form">
+        <h1>Gestion Multirubro</h1>
+        <form className="formulario">
+          <input type="text" autoComplete="off" required name="username" value={user} onChange={nameChange} placeholder="Usuario..." />
+          <input type="password" value={password} onChange={passwordChange} required placeholder="Contraseña..." />
+          <button type="submit" onClick={enviarFormulario}>Enviar</button>
+        </form>
+
+      </div>
     </div>
+
   );
 }
 
